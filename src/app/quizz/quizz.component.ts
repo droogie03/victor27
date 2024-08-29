eb060fd49e62f55a5e471cc2ed8748a16d298838
Julia Villalba <julia.v_28@hotmail.com>
2024-08-29 21:44:16 +0200

    -changes

--- src/app/quizz/quizz.component.ts
+++ src/app/quizz/quizz.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class QuizzComponent {
 
+  isExplanationClicked: boolean = false;
+
+  changeExplanationStatus():void{
+    this.isExplanationClicked = !this.isExplanationClicked;
+  }
+
 }
