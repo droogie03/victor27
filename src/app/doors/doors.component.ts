ebd402fefdd3ca66e86da9c2bc3d4463859956b1
Julia Villalba <julia.v_28@hotmail.com>
2024-09-01 19:28:49 +0200

    all done

--- src/app/doors/doors.component.ts
+++ src/app/doors/doors.component.ts
@@ -5,11 +5,43 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   templateUrl: './doors.component.html',
   styleUrls: ['./doors.component.css']
 })
-export class DoorsComponent {
+export class DoorsComponent implements OnInit{
 
   @Output() sustractOneLive: EventEmitter<void> = new EventEmitter<void>();
   @Output() showHangmanGame: EventEmitter<void> = new EventEmitter<void>();
   isExplanationClicked: boolean = false;
+  isCorrectAnswerSelected: boolean = false;
+  showAnswer: boolean = false;
+  doorsInformation: any[] = [
+    {
+      src:'../assets/closed-door.png', answer:false
+    },
+    {
+      src:'../assets/closed-door.png', answer:false
+    },
+    {
+      src:'../assets/closed-door.png', answer:false
+    }
+  ];
+
+  ngOnInit(): void {
+    let randomNumber = Math.floor(Math.random() * this.doorsInformation.length);
+    this.doorsInformation[randomNumber].answer = true;
+  }
+
+  isImageClicked(index: number): void{
+    if(this.doorsInformation[index].answer) this.isCorrectAnswerSelected = true;
+    this.doorsInformation[index].src = '../assets/open-door.png';
+    setTimeout(() => {
+      this.showAnswer = true;
+    }, 500);
+    
+  }
+
+  closeAnswerExplanation(){
+    if(!this.isCorrectAnswerSelected) this.sustractOneLive.emit();
+    this.showAnswer = false;
+  }
 
   changeExplanationStatus():void{
     this.isExplanationClicked = !this.isExplanationClicked;
